fa798638fb382d5c2b7e217817aee1eb21dc51c2
Richard Henry <r.henry1989@gmail.com>
2017-09-15 12:13:22 +0100

    adding in HMR publicPath to webcpack config output to enable react HMR

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,17 @@ const webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
-  entry: {
-    app: "./source/index.jsx"
-  },
+  entry: [
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:8080",
+    "webpack/hot/only-dev-server",
+    "./source/index.jsx"
+  ],
   devtool: "cheap-eval-source-map",
   output: {
     path: path.join(__dirname, "build"),
-    filename: "build.js"
+    filename: "build.js",
+    publicPath: "/"
   },
   devServer: {
     hot: true,
